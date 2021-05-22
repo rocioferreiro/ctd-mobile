8296dcc418ab5db4d499400254936d6a7bcdd4ed
Sofia Sidañez <sofia.sidanez@ing.austral.edu.ar>
2021-05-22 17:36:37 -0300

    Challenge mapping code clean up

--- components/ChallengeList/Challenge.tsx
+++ components/ChallengeList/Challenge.tsx
@@ -3,20 +3,10 @@ import {Text, View} from "../Themed";
 import React from "react";
 import {FIND_CHALLENGE_BY_ID} from "../apollo-graph/Queries";
 import {getApolloClientInstance} from "../apollo-graph/Client";
-import { ApolloClient, ApolloProvider, InMemoryCache ,useQuery} from '@apollo/client';
+import { ApolloProvider,useQuery} from '@apollo/client';
 
 
 export function ChallengeList() {
-    const uri='http://localhost:8080/graphql'
-    const query=FIND_CHALLENGE_BY_ID
-    const cache = new InMemoryCache();
-// Create a instance of Apollo Client
-  /*  const client = new ApolloClient({
-        uri,
-        cache,
-        headers: {},
-        connectToDevTools: process.env.NODE_ENV === 'development',
-    });*/
     const client= getApolloClientInstance()
 
     const {data,error,loading} = useQuery(FIND_CHALLENGE_BY_ID);
