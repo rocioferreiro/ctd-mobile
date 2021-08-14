f385a95f7a9278ca83fabd6d67c0cbd59be52bd8
AlejoRamirezGismondi <alejo.ramirezgismondi@ing.austral.edu.ar>
2021-08-14 17:56:08 -0300

    added new library for stepper

--- components/CreateChallengeForm/MyStepper.tsx
+++ components/CreateChallengeForm/MyStepper.tsx
@@ -1,6 +1,5 @@
-
-import React, { useState } from 'react';
-import { Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {Text, View} from 'react-native';
 
 import Stepper from 'react-native-stepper-ui';
 import ChallengeDetails from "./ChallengeDetails";
@@ -8,41 +7,30 @@ import ChallengeLocation from "./ChallengeLocation";
 import ChallengeExtraInfo from "./ChallengeExtraInfo";
 import ChallengePoints from "./ChallengePoints";
 
-/*const MyComponent = (props) => {
-  return (
-      <View>
-        <Text>{props.title}</Text>
-      </View>
-  );
-};*/
-
 const content = [
     <ChallengeDetails/>,
     <ChallengeLocation/>,
     <ChallengeExtraInfo/>,
     <ChallengePoints/>
- /* <MyComponent title="Component 1" />,
-  <MyComponent title="Component 2" />,
-  <MyComponent title="Component 3" />,*/
 ];
 
 const MyStepper = () => {
-  const [active, setActive] = useState(0);
+    const [active, setActive] = useState(0);
 
-  return (
-      <View style={{ marginVertical: 80, marginHorizontal: 20 }}>
-          <View style={{marginTop:20, marginBottom:20}}>
-          <Text> Create Challenge</Text>
-          </View>
-        <Stepper
-            active={active}
-            content={content}
-            onBack={() => setActive((p) => p - 1)}
-            onFinish={() => alert('Finish')}
-            onNext={() => setActive((p) => p + 1)}
-        />
-      </View>
-  );
+    return (
+        <View style={{marginVertical: 80, marginHorizontal: 20}}>
+            <View style={{marginTop: 20, marginBottom: 20}}>
+                <Text> Create Challenge </Text>
+            </View>
+            <Stepper
+                active={active}
+                content={content}
+                onBack={() => setActive((p) => p - 1)}
+                onFinish={() => alert('Finish')}
+                onNext={() => setActive((p) => p + 1)}
+            />
+        </View>
+    );
 };
 
 export default MyStepper;
