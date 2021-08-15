39fd3cfe4d806644aa540a0b1e8323cc81789292
Sofia Sidañez <sofia.sidanez@ing.austral.edu.ar>
2021-08-15 14:54:36 -0300

    wip challenge card

--- components/ChallengeCard/ChallengeCard.tsx
+++ components/ChallengeCard/ChallengeCard.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
-import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
+import {Avatar, Button, Card, Title, Paragraph, useTheme} from 'react-native-paper';
 import {Challenge} from "../Models/Challenge";
+import {StyleSheet} from "react-native";
+
+
 
 const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
 
@@ -8,19 +11,40 @@ interface Props {
     challenge: any;
 }
 
-const ChallengeCard = (props: Props) => (
-    <Card>
-        <Card.Title title="Username" subtitle="Level" left={LeftContent} />
-        <Card.Content>
-            <Title>{props.challenge.title}</Title>
-            <Paragraph>Challenge Description</Paragraph>
-        </Card.Content>
-        <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
-        <Card.Actions>
-            <Button>View</Button>
-            <Button>Join</Button>
-        </Card.Actions>
-    </Card>
-);
+const ChallengeCard = (props: Props) => {
+    const { colors } = useTheme();
+    const styles = StyleSheet.create({
+        joinButton: {
+            borderRadius: 20,
+            backgroundColor: colors.accent,
+            width: 60,
+            paddingLeft: 17,
+            marginBottom: 7
+        },
+        viewButton: {
+            borderRadius: 20,
+            width: 60,
+            paddingLeft: 17,
+            paddingRight: 17,
+            paddingTop: 8,
+            paddingBottom: 8,
+            backgroundColor: '#c1c1c1',
+        }
+    });
+    return (<Card>
+            <Card.Title title="Username" subtitle="Level" left={LeftContent}/>
+            <Card.Content>
+                <Title>{props.challenge.title}</Title>
+                <Paragraph>Challenge Description</Paragraph>
+            </Card.Content>
+            <Card.Cover source={{uri: 'https://picsum.photos/700'}}/>
+            <Card.Actions>
+                <Button style={styles.viewButton}>View</Button>
+                <Button style={styles.joinButton}>Join</Button>
+            </Card.Actions>
+        </Card>
+
+    );
+}
 
 export default ChallengeCard;
