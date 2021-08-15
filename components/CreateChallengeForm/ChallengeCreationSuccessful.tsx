fd048f353ad915c00de423a6b2132c448d6f2bf8
AlejoRamirezGismondi <alejo.ramirezgismondi@ing.austral.edu.ar>
2021-08-15 20:12:54 -0300

    added animation and styling

--- components/CreateChallengeForm/ChallengeCreationSuccessful.tsx
+++ components/CreateChallengeForm/ChallengeCreationSuccessful.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {View, Text} from "../Themed";
 import {Button, Card, useTheme} from "react-native-paper";
 import {StyleSheet} from "react-native";
+import LottieView from 'lottie-react-native';
 
 const ChallengeCreationSuccessful = () => {
     const { colors } = useTheme();
@@ -12,15 +13,39 @@ const ChallengeCreationSuccessful = () => {
             fontWeight: 'bold',
             color: colors.primary,
             marginLeft: 25,
-            marginTop: -10,
         },
+        checkmark: {
+            width: '100%',
+            height: 400,
+        },
+        buttonContainer: {
+            justifyContent: "center",
+            width: '100%',
+            alignItems: "center",
+            backgroundColor: 'rgba(0,0,0,0)'
+        },
+        button: {
+            width: '30%',
+            backgroundColor: colors.accent,
+            borderRadius: 30
+        }
     });
 
     return(
         <View>
             <Card>
                 <Text style={styles.title}>Challenge created!</Text>
-                <Button>Return</Button>
+                <LottieView
+                    style={styles.checkmark}
+                    source={require('../../assets/lottie/checkmark.json')}
+                    autoPlay
+                    loop
+                    speed={0.4}
+                    resizeMode={'cover'}
+                />
+                <View style={styles.buttonContainer}>
+                    <Button dark={false} style={styles.button} mode={'contained'}>Return</Button>
+                </View>
             </Card>
         </View>
     );
