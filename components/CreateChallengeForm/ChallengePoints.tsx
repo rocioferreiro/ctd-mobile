05d495d2e3b3e5345aa9e347a9c46c7c285dceae
AlejoRamirezGismondi <alejo.ramirezgismondi@ing.austral.edu.ar>
2021-08-21 19:48:42 -0300

    setted more fields

--- components/CreateChallengeForm/ChallengePoints.tsx
+++ components/CreateChallengeForm/ChallengePoints.tsx
@@ -3,6 +3,7 @@ import Slider from '@react-native-community/slider';
 import {Badge, Card, IconButton, useTheme} from "react-native-paper";
 import {View, Text} from "../Themed";
 import {Dimensions, StyleSheet, TextInput} from "react-native";
+import {ChallengeObjective} from "./Types";
 
 type Props = {
     formik: any
@@ -92,11 +93,21 @@ const ChallengePoints = (props: Props) => {
         }
     });
 
-    const onInputChanged = (value) => {
-        if (!value) setObjectivePoints("0");
+    const onInputChanged = (value, objective: ChallengeObjective) => {
+        if (!value) {
+            setObjectivePoints("0");
+            const currentObjectives = [...props.formik.values.challengeObjectives];
+            const changedObjective = currentObjectives.find(o => o.name == objective.name);
+            changedObjective.points = '0';
+            props.formik.setFieldValue('challengeObjectives', currentObjectives);
+        }
         const newValue = parseInt(value);
         if (newValue && newValue <= maxPoints && newValue >= minPoints) {
-            setObjectivePoints(`${newValue}`)
+            setObjectivePoints(`${newValue}`);
+            const currentObjectives = [...props.formik.values.challengeObjectives];
+            const changedObjective = currentObjectives.find(o => o.name == objective.name);
+            changedObjective.points = `${newValue}`;
+            props.formik.setFieldValue('challengeObjectives', currentObjectives);
         }
     }
 
@@ -116,7 +127,10 @@ const ChallengePoints = (props: Props) => {
                     minimumTrackTintColor={colors.primary}
                     maximumTrackTintColor={`rgba(${colors.primary}, 0.5)`}
                     thumbTintColor={colors.accent}
-                    onValueChange={(current) => setTotalPoints(current)}
+                    onValueChange={(current) => {
+                        props.formik.setFieldValue('totalPoints', current);
+                        setTotalPoints(current);
+                    }}
                 />
                 <View style={styles.sliderContainer}>
                     <Text style={styles.sliderText}>{minPoints}</Text>
@@ -124,29 +138,33 @@ const ChallengePoints = (props: Props) => {
                 </View>
                 <Text style={styles.subTitle}>Individual objective rewards</Text>
                 <View style={styles.objectivesContainer}>
-                    <View style={styles.objective}>
-                        <Text style={styles.objectiveText}>Objective 1</Text>
-                        <View style={styles.objectivePointsInput}>
-                            <TextInput keyboardType={"numeric"} value={objectivePoints} onChangeText={onInputChanged}
-                                       style={{fontSize: 14, color: colors.primary, fontWeight: "bold"}}/>
-                            <View style={styles.buttonsContainer}>
-                                <IconButton
-                                    icon="minus"
-                                    color={colors.primary}
-                                    size={30}
-                                    onPress={() => onInputChanged(`${parseInt(objectivePoints) - 1}`)}
-                                    style={{padding: 0, margin: 0, marginRight: -10}}
-                                />
-                                <IconButton
-                                    icon="plus"
-                                    color={colors.primary}
-                                    size={30}
-                                    onPress={() => onInputChanged(`${parseInt(objectivePoints) + 1}`)}
-                                    style={{padding: 0, margin: 0}}
-                                />
-                            </View>
-                        </View>
-                    </View>
+                    {props.formik.values.challengeObjectives.map((objective: ChallengeObjective, index) => {
+                        return (
+                            <View key={index} style={styles.objective}>
+                                <Text style={styles.objectiveText}>{objective.name}</Text>
+                                <View style={styles.objectivePointsInput}>
+                                    <TextInput keyboardType={"numeric"} value={'' + objective.points}
+                                               onChangeText={(value) => onInputChanged(value, objective)}
+                                               style={{fontSize: 14, color: colors.primary, fontWeight: "bold"}}/>
+                                    <View style={styles.buttonsContainer}>
+                                        <IconButton
+                                            icon="minus"
+                                            color={colors.primary}
+                                            size={30}
+                                            onPress={() => onInputChanged(`${parseInt(objectivePoints) - 1}`, objective)}
+                                            style={{padding: 0, margin: 0, marginRight: -10}}
+                                        />
+                                        <IconButton
+                                            icon="plus"
+                                            color={colors.primary}
+                                            size={30}
+                                            onPress={() => onInputChanged(`${parseInt(objectivePoints) + 1}`, objective)}
+                                            style={{padding: 0, margin: 0}}
+                                        />
+                                    </View>
+                                </View>
+                            </View>)
+                    })}
                 </View>
             </Card>
         </View>
