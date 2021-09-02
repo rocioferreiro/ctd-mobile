869f45751864de3826abb481727df0ddcf2eb9e8
Sofia Sidañez <sofia.sidanez@ing.austral.edu.ar>
2021-09-02 11:50:04 -0300

    wip create post

---
+++ components/CreatePost/CreatePost.tsx
@@ -0,0 +1,27 @@
+import React, {useEffect, useState} from "react";
+
+import {Card, Divider, useTheme} from 'react-native-paper';
+import {View,Text} from "../Themed";
+
+
+
+const CreatePost = () => {
+
+    const { colors } = useTheme();
+    const onChange = (searchValue: string) => {
+
+        }
+
+
+    return (
+        <View>
+
+        <Text>Create Post Screen</Text>
+
+        </View>
+
+    )
+};
+
+
+export default CreatePost;
