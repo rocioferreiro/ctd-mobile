cd2109658b97420eeed5899346b38dfec81b4d33
Sofia Sidañez <sofia.sidanez@ing.austral.edu.ar>
2021-09-02 14:39:43 -0300

    added text input

--- components/CreatePost/CreatePost.tsx
+++ components/CreatePost/CreatePost.tsx
@@ -1,22 +1,39 @@
 import React, {useEffect, useState} from "react";
 
-import {Card, Divider, useTheme} from 'react-native-paper';
+import {Button, Card, Divider, useTheme} from 'react-native-paper';
 import {View,Text} from "../Themed";
+import {Dimensions, ScrollView, StyleSheet} from "react-native";
+import {Input} from "react-native-elements";
+import {colorShade} from "../Models/shadingColor";
+import PostTextInput from "./PostTextInput";
+type Props = {
+
+    formik: any
+}
 
 
-
-const CreatePost = () => {
-
+const CreatePost = (props:Props) => {
+    const {formik} = props;
     const { colors } = useTheme();
+
     const onChange = (searchValue: string) => {
 
         }
 
 
-    return (
-        <View>
 
-        <Text>Create Post Screen</Text>
+
+    return (
+        <View style={{flex:1,width:Dimensions.get("screen").width, height:Dimensions.get("window").height * 0.1,backgroundColor:colors.surface}}>
+            <View style={{width:"100%",alignItems:"flex-start" ,padding:10,marginTop: 20,backgroundColor:colors.surface}}>
+                <Button icon="keyboard-backspace" >
+                    Back
+                </Button>
+        <Text>Create Post</Text>
+                <ScrollView>
+                <PostTextInput></PostTextInput>
+                </ScrollView>
+            </View>
 
         </View>
 
