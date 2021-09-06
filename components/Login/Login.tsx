07fc7cccf598d5b565a36fa943c7c67a77af0312
AlejoRamirezGismondi <alejo.ramirezgismondi@ing.austral.edu.ar>
2021-09-06 00:10:15 -0300

    Finished awesome animation

--- components/Login/Login.tsx
+++ components/Login/Login.tsx
@@ -1,17 +1,28 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, View} from "../Themed";
 import {Dimensions, Image, StyleSheet} from "react-native";
 import {Input} from "react-native-elements";
 import {Button, useTheme} from "react-native-paper";
+import LottieView from "lottie-react-native";
+import Animated, {useAnimatedStyle, useSharedValue, withTiming} from "react-native-reanimated";
 
 type Props = {
     onCancel: () => void
 }
 
 const Login = (props: Props) => {
-    const { colors } = useTheme();
+    const {colors} = useTheme();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [animationFinished, setAnimationFinished] = useState(false);
+    const visible = useSharedValue(3);
+
+    const reanimatedStyle = useAnimatedStyle(() => {
+        return {
+            opacity: visible.value,
+            zIndex: visible.value
+        }
+    }, [])
 
     const styles = StyleSheet.create({
         root: {
@@ -56,35 +67,72 @@ const Login = (props: Props) => {
         logo: {
             height: 100,
             width: 100,
+        },
+        animatedContainer: {
+            width: Dimensions.get('screen').width,
+            height: Dimensions.get('screen').height,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'rgba(0,0,0,0)',
+            position: "absolute",
         }
-    })
+    });
+
+    useEffect(() => {
+        visible.value = withTiming(0, {duration: 1000});
+    }, [])
 
     return (
         <View style={styles.root}>
-            <Image source={require('../../assets/images/ctd-logo.jpg')} style={styles.logo}/>
-            <Text style={styles.title}>Login</Text>
-            <Input
-                placeholder={"Email"}
-                style={styles.input}
-                value={email}
-                onChangeText={t => {
-                    setEmail(t);
-                }}
-                inputContainerStyle={{borderBottomWidth: 0}}
-            />
-            <Input
-                placeholder={"Password"}
-                style={styles.input}
-                value={password}
-                onChangeText={t => {
-                    setPassword(t);
-                }}
-                inputContainerStyle={{borderBottomWidth: 0}}
-            />
-            <View style={{height: 150, backgroundColor: 'rgba(0,0,0,0)', display: "flex", justifyContent: 'space-around'}}>
-                <Button style={styles.button} mode={'contained'} onPress={() => {console.log('login')}}>Login</Button>
-                <Button style={styles.cancelButton} mode={'contained'} onPress={() => {props.onCancel()}}>Cancel</Button>
+            <Animated.View style={[styles.animatedContainer, reanimatedStyle]}>
+                <LottieView
+                    source={require('../../assets/lottie/liquid-transition.json')}
+                    autoPlay
+                    loop={false}
+                    speed={3}
+                    resizeMode={'cover'}
+                    onAnimationFinish={() => {
+                        setAnimationFinished(true)
+                    }}
+                />
+            </Animated.View>
+            {animationFinished &&
+                <View style={styles.root}>
+                <Image source={require('../../assets/images/ctd-logo.jpg')} style={styles.logo}/>
+                <Text style={styles.title}>Login</Text>
+                <Input
+                    placeholder={"Email"}
+                    style={styles.input}
+                    value={email}
+                    onChangeText={t => {
+                        setEmail(t);
+                    }}
+                    inputContainerStyle={{borderBottomWidth: 0}}
+                />
+                <Input
+                    placeholder={"Password"}
+                    style={styles.input}
+                    value={password}
+                    onChangeText={t => {
+                        setPassword(t);
+                    }}
+                    inputContainerStyle={{borderBottomWidth: 0}}
+                />
+                <View style={{
+                    height: 150,
+                    backgroundColor: 'rgba(0,0,0,0)',
+                    display: "flex",
+                    justifyContent: 'space-around'
+                }}>
+                    <Button style={styles.button} mode={'contained'} onPress={() => {
+                        console.log('login')
+                    }}>Login</Button>
+                    <Button style={styles.cancelButton} mode={'contained'} onPress={() => {
+                        props.onCancel()
+                    }}>Cancel</Button>
+                </View>
             </View>
+            }
         </View>
     );
 }
