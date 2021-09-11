d05b76a8ad58ab60f771482599b78d54395de58b
rocioferreiro <rocio.ferreirorico@ing.austral.edu.ar>
2021-09-11 16:50:11 -0300

    post view finished

--- components/viewPost/ViewPost.tsx
+++ components/viewPost/ViewPost.tsx
@@ -1,24 +1,37 @@
 import {Avatar, Button, Card, Colors, Paragraph, Title, useTheme} from "react-native-paper";
-import React from "react";
+import OptionsMenu from "react-native-options-menu";
+import React, {useRef} from "react";
 import {Post} from "../Models/Post";
 import {Icon} from "react-native-elements";
 import {Text, View} from "../Themed";
+import { captureRef } from 'react-native-view-shot';
+import {PixelRatio} from "react-native";
+import * as MediaLibrary from 'expo-media-library';
 
 type Props = {
-  post: Post
+  post: Post,
+  open: boolean
 }
-
-
 
 const ViewPost = (props:Props) => {
   const { colors } = useTheme();
-  const [image, setImage] = React.useState(null)
-  const [addImage, setAddImage] = React.useState(false)
   const [liked, setLiked] = React.useState(false)
   const {post} = props;
+  const [likes, setLikes] = React.useState(post.upVotes)
 
+  const likePost = (isLiking: boolean)  => {
+    //TODO implement like post
+    setLiked(!liked)
+    isLiking? setLikes(likes+1) : setLikes(likes-1)
+  }
+
+  const myIcon = <Icon type={'ionicon'} name={'ellipsis-horizontal'} style={{marginRight: 10}} {...props}/>
   const LeftContent = props => <Avatar.Text style={{width: 50, height: 50, borderRadius: '50%', backgroundColor: colors.extra}} label={post.owner.name[0] + post.owner.lastname[0] } {...props}/>
-  const RightContent = props => <Icon type={'ionicon'} name={'ellipsis-horizontal'} style={{marginRight: 10}} {...props}/>
+  const RightContent = props => <OptionsMenu
+                                    customButton={myIcon}
+                                    destructiveIndex={0}
+                                    options={["Report", "Copy Link", "Disconnect", "Cancel"]}
+                                    actions={[()=>{console.log("TODO Report Post")}, ()=>{console.log("TODO Copy Link")}, ()=>{console.log("TODO Disconnect to user")},()=>{}]}/>
 
   return (
     <Card style={{backgroundColor: colors.background, borderRadius: 20, marginHorizontal: 10, marginTop: 10}}>
@@ -36,14 +49,14 @@ const ViewPost = (props:Props) => {
       </Card.Content>
       {(post.image && post.image !== "") && <Card.Cover style={{marginHorizontal: 15, borderRadius: 20}} source={{uri: 'https://picsum.photos/700'}}/>}
       <Card.Actions style={{width: '100%', display:'flex', justifyContent:'space-between', marginVertical: 10}}>
-        <View style={{display:'flex', flexDirection:'row', alignItems: 'center', marginLeft: 15}}>
-          <Icon name={liked ? 'heart' : 'heart-outline'} type={'material-community'} onPress={() => setLiked(!liked)}/>
-          <Text style={{marginRight: 10}}> {post.upVotes} </Text>
-          <Icon name={'chat-outline'} type={'material-community'} onPress={() => {}}/>
-          <Text> 1 </Text>
+        <View style={{display:'flex', flexDirection:'row', alignItems: 'center', marginLeft: 15, backgroundColor:'rgba(0,0,0,0)'}}>
+          <Icon name={liked ? 'heart' : 'heart-outline'} type={'material-community'} style={{color: colors.primary}} onPress={() => likePost(!liked)}/>
+          <Text style={{marginRight: 10, color: colors.primary}}> {likes} </Text>
+          <Icon name={'chat-outline'} type={'material-community'} style={{color: colors.primary}} onPress={() => {}}/>
+          <Text style={{color: colors.primary}}> 1 </Text>
         </View>
-        <View style={{marginRight:15}}>
-          <Icon name={'share-variant'} type={'material-community'} onPress={() => {}}/>
+        <View style={{marginRight:15, backgroundColor:'rgba(0,0,0,0)'}}>
+          <Icon name={'share-variant'} style={{color: colors.primary}} type={'material-community'} onPress={() => {}}/>
         </View>
       </Card.Actions>
     </Card>
