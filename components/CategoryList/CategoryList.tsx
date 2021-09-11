895445b3c2e170cbf1a377512cd4a3937c5b36f5
AlejoRamirezGismondi <alejo.ramirezgismondi@ing.austral.edu.ar>
2021-09-11 20:18:44 -0300

    most styling done. Missing pictures

--- components/CategoryList/CategoryList.tsx
+++ components/CategoryList/CategoryList.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import {View, Text} from "../Themed";
-import {onuLogos, ONUObjectives} from "../ONUObjectives";
-import {Card} from "react-native-paper";
-import {Dimensions, Image, StyleSheet} from "react-native";
+import {colors, onuLogos, ONUObjectives} from "../ONUObjectives";
+import {Card, useTheme} from "react-native-paper";
+import {Dimensions, Image, ScrollView, StyleSheet} from "react-native";
 
 const CategoryList = () => {
 
@@ -12,27 +12,66 @@ const CategoryList = () => {
         },
         card: {
             display: 'flex',
+            flexDirection: 'row',
             borderRadius: 5,
             width: Dimensions.get('window').width * 0.8,
             height: Dimensions.get('window').height * 0.15,
             marginBottom: 10,
-            marginTop: 10
+            marginTop: 10,
+            overflow: 'hidden'
+        },
+        image: {
+
+        },
+        sideNumberContainer: {
+            flex: 2,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: 5
+        },
+        number: {
+            fontSize: 35,
+            fontWeight: 'bold',
+            color: '#ffffff'
+        },
+        sideImageContainer: {
+            backgroundColor: 'rgba(0,0,0,0)',
+            flex: 10,
+            display: 'flex',
+            justifyContent: 'flex-end',
+            borderRadius: 5,
+            overflow: 'hidden'
+        },
+        name: {
+            fontWeight: 'bold',
+            color: '#ffffff',
+            marginLeft: 6,
+            marginBottom: 6,
         },
     });
 
     return (
-        <View style={styles.container}>
+        <ScrollView style={styles.container}>
             {Object.values(ONUObjectives).map((v, i) => {
                 return (
-                    <Card style={styles.card} key={i}>
-                        <View>
-                            <Text>{i}</Text>
-                            <Image style={styles.image} source={onuLogos[i].image}/>
+                    <View style={styles.card} key={i}>
+                        <View style={[styles.sideNumberContainer, {backgroundColor: colors[i]}]}>
+                            <Text style={styles.number}>{i+1}</Text>
+                            {/*<Image style={styles.image} source={onuLogos[i].image}/>*/}
                         </View>
-                    </Card>
+                        <View style={styles.sideImageContainer}>
+                            <View style={{
+                                ...StyleSheet.absoluteFillObject,
+                                backgroundColor: colors[i],
+                                opacity: 0.5
+                            }} />
+                            <Text style={styles.name}>{v}</Text>
+                        </View>
+                    </View>
                 )
             })}
-        </View>
+        </ScrollView>
     );
 }
 export default CategoryList;
