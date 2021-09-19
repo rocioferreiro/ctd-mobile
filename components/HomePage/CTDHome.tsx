de7dcf83dfa6eb0eae0add1f55805f4a03a1f24f
Sofia Sidañez <sofia.sidanez@ing.austral.edu.ar>
2021-09-19 13:09:02 -0300

    added logo to homescreen

--- components/HomePage/CTDHome.tsx
+++ components/HomePage/CTDHome.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import {Dimensions,StyleSheet} from "react-native";
+import {Dimensions, Image, ScrollView, StyleSheet} from "react-native";
 import {Button, useTheme} from "react-native-paper";
 import {View, Text} from "../Themed";
 
@@ -18,6 +18,16 @@ const CTDHome = () => {
             backgroundColor: colors.surface,
             width: Dimensions.get('window').width,
             height: Dimensions.get('window').height,
+        },
+        title: {
+            backgroundColor: 'rgba(0,0,0,0)',
+            fontSize: 30,
+            color: '#ffffff',
+            fontWeight: 'bold'
+        },
+        logo: {
+            height: 110,
+            width: 110,
         }
     });
 
@@ -27,8 +37,16 @@ const CTDHome = () => {
 
     return (
         <View style={styles.container}>
-            <Text> HOME SCREEN</Text>
-        </View>
+            <View style={{flex:1,width:Dimensions.get("screen").width, height:Dimensions.get("window").height * 0.1,backgroundColor:colors.surface}}>
+                <ScrollView  contentContainerStyle={{ flexGrow:1, justifyContent: "center", width: '100%'}} style={{ flex: 1,backgroundColor:"rgba(0,0,0,0)"}}  >
+                    <View style={{width:"90%",justifyContent: "center", alignItems: "center", padding:10, backgroundColor:colors.surface}}>
+                    <Image resizeMode={"contain"} source={require('../../assets/images/ctd-logo.png')} style={styles.logo}/>
+                    <Text style={styles.title}>Connect the Dots</Text>
+                    </View>
+                </ScrollView>
+            </View>
+
+            </View>
     )
 }
 
