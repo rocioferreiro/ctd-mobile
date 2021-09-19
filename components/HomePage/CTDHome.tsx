515655dfa952b7d29d8b92f92278ad2a5f1e0033
Sofia Sidañez <sofia.sidanez@ing.austral.edu.ar>
2021-09-19 13:20:31 -0300

    added global sustainable points to home screen

--- components/HomePage/CTDHome.tsx
+++ components/HomePage/CTDHome.tsx
@@ -21,9 +21,19 @@ const CTDHome = () => {
         },
         title: {
             backgroundColor: 'rgba(0,0,0,0)',
-            fontSize: 30,
+            fontSize: 20,
             color: '#ffffff',
             fontWeight: 'bold'
+        },
+        subtitle: {
+            backgroundColor: 'rgba(0,0,0,0)',
+            fontSize: 30,
+            color: colors.accent,
+            fontWeight: 'bold'
+        },
+        detailtitle: {
+            backgroundColor: 'rgba(0,0,0,0)',
+            color: '#ffffff',
         },
         logo: {
             height: 110,
@@ -39,9 +49,18 @@ const CTDHome = () => {
         <View style={styles.container}>
             <View style={{flex:1,width:Dimensions.get("screen").width, height:Dimensions.get("window").height * 0.1,backgroundColor:colors.surface}}>
                 <ScrollView  contentContainerStyle={{ flexGrow:1, justifyContent: "center", width: '100%'}} style={{ flex: 1,backgroundColor:"rgba(0,0,0,0)"}}  >
-                    <View style={{width:"90%",justifyContent: "center", alignItems: "center", padding:10, backgroundColor:colors.surface}}>
+                    <View style={{justifyContent: "center", alignItems: "center", padding:10, backgroundColor:colors.surface}}>
+                    <View style={{width:"70%",justifyContent: "center", alignItems: "center", padding:10, backgroundColor:colors.surface}}>
+
                     <Image resizeMode={"contain"} source={require('../../assets/images/ctd-logo.png')} style={styles.logo}/>
                     <Text style={styles.title}>Connect the Dots</Text>
+                        </View>
+                    </View>
+                    <View style={{justifyContent: "center", alignItems: "center", padding:10, backgroundColor:colors.surface}}>
+                    <View style={{width:"60%",justifyContent: "center", alignItems: "center", padding:15, backgroundColor:colors.primary,borderRadius:20}}>
+                        <Text style={styles.subtitle}>36500k</Text>
+                        <Text style={styles.detailtitle}> Global Sustainable Points</Text>
+                    </View>
                     </View>
                 </ScrollView>
             </View>
