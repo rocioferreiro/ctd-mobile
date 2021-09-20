6e9a51cdc1d6149c42e18c4bb119fd294679494f
Sofia Sidañez <sofia.sidanez@ing.austral.edu.ar>
2021-09-20 14:44:59 -0300

    translations ready for join button and challenge card mini

--- components/Challenge/JoinButton.tsx
+++ components/Challenge/JoinButton.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import {Button, Title, useTheme} from 'react-native-paper';
+import {useTranslation} from "react-i18next";
 
 
 const JoinButton = () => {
+    const {t, i18n} = useTranslation();
+    const [language, setLanguage] = React.useState(i18n.language);
     const { colors } = useTheme();
    return( <Button style={{ backgroundColor:colors.primary, height:70, width:"100%", padding:15,justifyContent: "center",alignContent:"center"}} mode="contained" onPress={() => console.log('Pressed')}>
-      <Title style={{fontSize:22,fontWeight:"bold", color:colors.background}}>Join this Challenge
-      <Title style={{fontSize:22,fontWeight:"bold", color:colors.accent}}> Now</Title>
+      <Title style={{fontSize:22,fontWeight:"bold", color:colors.background}}>{t('join-button.join-this-challenge')}
+      <Title style={{fontSize:22,fontWeight:"bold", color:colors.accent}}> {t('join-button.now')}</Title>
       </Title>
     </Button>
 );
--- components/ChallengeCard/ChallengesCardMini.tsx
+++ components/ChallengeCard/ChallengesCardMini.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import {Avatar, Button, Card, Title, Paragraph, useTheme} from 'react-native-paper';
 import {Challenge} from "../Models/Challenge";
 import {StyleSheet,Text} from "react-native";
+import {useTranslation} from "react-i18next";
 
 
 
@@ -14,6 +15,8 @@ interface Props {
 
 const ChallengeCardMini = (props: Props) => {
     const { colors } = useTheme();
+    const {t, i18n} = useTranslation();
+    const [language, setLanguage] = React.useState(i18n.language);
     const styles = StyleSheet.create({
         joinButton: {
             borderRadius: 20,
@@ -40,15 +43,15 @@ const ChallengeCardMini = (props: Props) => {
             <Card.Content>
                 <Title style={{ fontSize: 25, color: colors.background,
                     marginTop: 5}}>{props.challenge.title}</Title>
-                <Paragraph style={{ color: colors.background,fontSize:15,marginBottom:5}}>Challenge Description</Paragraph>
+                <Paragraph style={{ color: colors.background,fontSize:15,marginBottom:5}}>{t('challenge-card-mini.challenge-description')}</Paragraph>
             </Card.Content>
             <Card.Cover source={{uri: 'https://picsum.photos/700'}}/>
             <Card.Actions>
                 <Button style={ {backgroundColor:  '#c1c1c1', borderRadius: 20,  width: 100,marginLeft:40,marginRight:120,}}>
                     <Title style={{ fontSize: 15, color: colors.primary,
-                    }}>View</Title> </Button>
+                    }}>{t('challenge-card-mini.view')}</Title> </Button>
                 <Button style={ {backgroundColor: colors.accent, borderRadius: 20,  width: 100, }} > <Title style={{ fontSize: 15, color: colors.primary,
-                }}>Share</Title></Button>
+                }}>{t('challenge-card-mini.share')}</Title></Button>
             </Card.Actions>
         </Card>
 
