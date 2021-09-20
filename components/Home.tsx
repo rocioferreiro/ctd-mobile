fe86d2efe92fc5d4016a0cfd89c50ac70840d232
Sofia Sidañez <sofia.sidanez@ing.austral.edu.ar>
2021-09-20 10:52:45 -0300

    merge with dev done

--- components/Home.tsx
+++ components/Home.tsx
@@ -14,6 +14,7 @@ import CreatePost from "./CreatePost/CreatePost";
 import ViewPost from "./viewPost/ViewPost";
 import {Role} from "./Models/User";
 import {getUserId} from "./Storage";
+import {useTranslation} from "react-i18next";
 
 const Home = () => {
 
