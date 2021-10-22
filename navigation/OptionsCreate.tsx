0df9994cf18f26d014427073ce4755bedd7bc146
AlejoRamirezGismondi <alejo.ramirezgismondi@ing.austral.edu.ar>
2021-10-22 20:04:04 -0300

    divided page

--- navigation/OptionsCreate.tsx
+++ navigation/OptionsCreate.tsx
@@ -1,20 +1,40 @@
 import React from 'react';
 import {Text, View} from "../components/Themed";
 import {TouchableOpacity} from "react-native-gesture-handler";
-import {Surface} from "react-native-paper";
-import {Dimensions, StyleSheet} from "react-native";
+import {useTheme} from "react-native-paper";
+import {Dimensions, Image, StyleSheet} from "react-native";
+import {categoryBackgrounds} from "../components/ONUObjectives";
 
 const OptionsCreate = () => {
 
   const windowHeight = Dimensions.get('window').height;
   const windowWidth = Dimensions.get('window').width;
+  const {colors} = useTheme();
 
   const styles = StyleSheet.create({
-    surface: {
-      height: windowHeight / 4,
-      width: windowWidth / 1.5,
-      backgroundColor: 'black',
-      borderRadius: 15
+    card: {
+      display: 'flex',
+      borderRadius: 5,
+      height: windowHeight / 3,
+      width: windowWidth * 0.9,
+      marginBottom: 10,
+      marginTop: 10,
+      overflow: 'visible',
+      backgroundColor: 'rgba(0,0,0,0)'
+    },
+    imageContainer: {
+      backgroundColor: 'rgba(0,0,0,0)',
+      flex: 10,
+      display: 'flex',
+      justifyContent: 'flex-end',
+      borderRadius: 15,
+      overflow: 'hidden'
+    },
+    name: {
+      fontWeight: 'bold',
+      color: '#ffffff',
+      marginLeft: 6,
+      marginBottom: 6,
     },
   });
 
@@ -22,20 +42,42 @@ const OptionsCreate = () => {
     <View style={{
       height: windowHeight,
       width: windowWidth,
-      backgroundColor: 'transparent',
+      backgroundColor: colors.surface,
       alignItems: 'center',
       justifyContent: 'space-around',
       paddingVertical: 40
     }}>
-      <TouchableOpacity>
-        <Surface style={styles.surface}>
-          <Text>Create Post</Text>
-        </Surface>
+      <TouchableOpacity onPress={() => {
+
+      }}>
+        <View style={styles.card}>
+          <View style={[styles.imageContainer, {backgroundColor: 'blue'}]}>
+            <Image style={{
+              ...StyleSheet.absoluteFillObject,
+              backgroundColor: 'blue',
+              opacity: 0.5,
+              width: '100%',
+              height: '100%'
+            }} source={categoryBackgrounds[0].image} resizeMode={'cover'}/>
+            <Text style={styles.name}>Create Post</Text>
+          </View>
+        </View>
       </TouchableOpacity>
-      <TouchableOpacity>
-        <Surface style={styles.surface}>
-          <Text>Create Challenge</Text>
-        </Surface>
+      <TouchableOpacity onPress={() => {
+
+      }}>
+        <View style={styles.card}>
+          <View style={[styles.imageContainer, {backgroundColor: 'red'}]}>
+            <Image style={{
+              ...StyleSheet.absoluteFillObject,
+              backgroundColor: 'red',
+              opacity: 0.5,
+              width: '100%',
+              height: '100%'
+            }} source={categoryBackgrounds[1].image} resizeMode={'cover'}/>
+            <Text style={styles.name}>Create Challenge</Text>
+          </View>
+        </View>
       </TouchableOpacity>
     </View>
   )
