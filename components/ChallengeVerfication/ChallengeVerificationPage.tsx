8e3736147afc56f721ae833a7c6f5bfb1cbec8ce
Sofia Sidañez <sofia.sidanez@ing.austral.edu.ar>
2021-10-22 11:39:19 -0300

    used challenge creation success as template

--- components/ChallengeVerfication/ChallengeVerificationPage.tsx
+++ components/ChallengeVerfication/ChallengeVerificationPage.tsx
@@ -1,19 +1,71 @@
-import * as React from 'react';
-import {Button, Title, useTheme} from 'react-native-paper';
+
+import React from 'react';
+import {View, Text} from "../Themed";
+import {Button, Card, useTheme} from "react-native-paper";
+import {Dimensions, StyleSheet} from "react-native";
+import LottieView from 'lottie-react-native';
 import {useTranslation} from "react-i18next";
-import {View} from "../Themed";
 
-interface Props {
-
+type Props = {
+    close: () => void
 }
 
-const ChallengeVerificationPage = (props: Props) => {
-    const {t, i18n} = useTranslation();
+const ChallengeCreationSuccessful = (props: Props) => {
     const { colors } = useTheme();
-    return(<View>
-            <Title>Challenge Verification Page</Title>
-        </View>
+    const {t, i18n} = useTranslation();
+
+    const styles = StyleSheet.create({
+        container: {
+            height: Dimensions.get('window').height,
+            paddingTop: Dimensions.get('window').height * 0.2,
+            alignContent: 'center'
+        },
+        title: {
+            fontSize: 35,
+            fontWeight: 'bold',
+            color: colors.primary,
+            textAlign: 'center'
+
+        },
+        checkmark: {
+            width: '100%',
+            height: 400,
+        },
+        buttonContainer: {
+            justifyContent: "center",
+            width: '100%',
+            alignItems: "center",
+            backgroundColor: 'rgba(0,0,0,0)'
+        },
+        button: {
+            width: '30%',
+            backgroundColor: colors.accent,
+            borderRadius: 30
+        }
+    });
+
+    return(
+        <Card style={styles.container}>
+            <View style={{backgroundColor: 'rgba(0,0,0,0)'}}>
+                <Text style={styles.title}>Challenge created!</Text>
+                <LottieView
+                    style={styles.checkmark}
+                    source={require('../../assets/lottie/checkmark.json')}
+                    autoPlay
+                    loop
+                    speed={0.4}
+                    resizeMode={'cover'}
+                />
+                <View style={styles.buttonContainer}>
+                    <Button dark={false} style={styles.button} mode={'contained'} onPress={props.close}>Return</Button>
+                </View>
+            </View>
+        </Card>
+
     );
 }
 
 export default ChallengeVerificationPage;
+
+
+
