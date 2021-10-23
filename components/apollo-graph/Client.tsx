31565349ba3229b2bd46e73e368d9f98ea7dde1a
AlejoRamirezGismondi <alejo.ramirezgismondi@ing.austral.edu.ar>
2021-10-23 03:49:18 -0300

    implemented auth interceptor

--- components/apollo-graph/Client.tsx
+++ components/apollo-graph/Client.tsx
@@ -11,24 +11,27 @@ export function getApolloClientInstance(): ApolloClient<object> {
   const uri = 'http://192.168.1.101:8080/graphql';
 
   const httpLink = new HttpLink({
-      uri: uri,
-      headers: {},
-      useGETForQueries: false
-    });
+    uri: uri,
+    headers: {},
+    useGETForQueries: false
+  });
 
-  // const headerLink = setContext(async (request, previousContext) => {
-  //   getToken().then(token => {
-  //     return {
-  //       headers: {
-  //         ...previousContext.headers,
-  //         'Authorization': 'Bearer ' + token,
-  //       },
-  //     }
-  //   });
-  // });
+  const authLink = setContext(async (request, previousContext) => {
+
+    const token = await getToken();
+
+    return(
+      {
+        headers: {
+          ...previousContext.headers,
+          'Authorization': token ? `Bearer ${token}` : "",
+        },
+      }
+    );
+  });
 
   return new ApolloClient<object>({
-    link: httpLink,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
     connectToDevTools: process.env.NODE_ENV === 'development',
   });
