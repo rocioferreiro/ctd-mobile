df42b1188ffacb39cd37b50ebcd72e67fbed855d
AlejoRamirezGismondi <alejo.ramirezgismondi@ing.austral.edu.ar>
2021-10-30 16:27:49 -0300

    removed clientid

--- ClientId.tsx
+++ ClientId.tsx
@@ -1,10 +1,2 @@
 export const androidClientId = '746337143443-o7i5sscrcv8n70g445an6fc6orcagco9.apps.googleusercontent.com';
 export const iOSClientId = '746337143443-uph0bsq7i5sthtddijmn217qlr20edti.apps.googleusercontent.com';
-export const firebaseConfig = {
-  apiKey: "AIzaSyBwuFdlh-Zl-lcHNVWujLsEG_nlRhiYj4s",
-  authDomain: "connect-the-dots-323522.firebaseapp.com",
-  projectId: "connect-the-dots-323522",
-  storageBucket: "connect-the-dots-323522.appspot.com",
-  messagingSenderId: "746337143443",
-  appId: "1:746337143443:web:b32a00659d238bf6b15b66"
-};
