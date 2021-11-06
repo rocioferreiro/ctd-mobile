64cdcab0bfe538b680c7df00b6967274b7d3a4a2
AlejoRamirezGismondi <alejo.ramirezgismondi@ing.austral.edu.ar>
2021-11-06 01:32:10 -0300

    profile background pic

--- components/Profile/Profile.tsx
+++ components/Profile/Profile.tsx
@@ -692,8 +692,8 @@ export function Profile(props: Props) {
       <ScrollView>
 
           <ImageBackground style={styles.profileBackground}
-            //imageStyle={{borderTopLeftRadius: 12, borderTopRightRadius: 12}}
-                           source={require('../../assets/images/profile-background.jpg')} resizeMode={'cover'}>
+                           source={{uri: 'https://images.theconversation.com/files/393210/original/file-20210401-13-z6rl6z.jpg?ixlib=rb-1.1.0&rect=0%2C292%2C3008%2C1504&q=45&auto=format&w=1356&h=668&fit=crop'}}
+                           resizeMode={'cover'}>
             {props.route.params?.otherId && <View style={styles.imageCoverContainer}>
                 <IconButton onPress={() => props.navigation.goBack()} style={{marginTop: 50}} icon={'chevron-left'}/>
                 <Button2 icon="plus"
