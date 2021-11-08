bb272b0d2c27505c50fad70eb5316494722a7671
AlejoRamirezGismondi <alejo.ramirezgismondi@ing.austral.edu.ar>
2021-11-07 21:48:46 -0300

    formatting

--- components/viewPost/ViewPost.tsx
+++ components/viewPost/ViewPost.tsx
@@ -1,10 +1,10 @@
-import {Avatar, Card, Divider, IconButton, Paragraph, useTheme} from "react-native-paper";
+import {Avatar, Card, IconButton, Paragraph, useTheme} from "react-native-paper";
 import OptionsMenu from "react-native-options-menu";
 import React, {useState, useEffect} from "react";
 import {Post} from "../Models/Post";
 import {Icon} from "react-native-elements";
 import {Text, View} from "../Themed";
-import {Dimensions, ScrollView, StyleSheet, TouchableOpacity} from "react-native";
+import {Dimensions, ScrollView, TouchableOpacity} from "react-native";
 import {useTranslation} from "react-i18next";
 import {useLazyQuery} from "@apollo/client";
 import {FIND_POST_BY_ID, NEW_FIND_USER_BY_ID} from "../apollo-graph/Queries";
@@ -14,8 +14,6 @@ import {LIKE_POST, UNLIKE_POST} from "../apollo-graph/Mutations";
 import Clipboard from 'expo-clipboard';
 import {share} from "../Share";
 import * as Linking from 'expo-linking';
-import {ip} from "../apollo-graph/Client";
-import ChallengeCard from "../ChallengeCard/ChallengeCard";
 
 type Props = {
   post?: Post,
@@ -23,21 +21,20 @@ type Props = {
   open: boolean,
   route?: any,
   navigation?: any
-  key:number
+  key: number
 }
 
-const ViewPost = (props:Props) => {
-  const { colors } = useTheme();
+const ViewPost = (props: Props) => {
+  const {colors} = useTheme();
   const [userId, setUserId] = React.useState('');
   const [liked, setLiked] = React.useState(false);
   const [owner, setOwner] = React.useState<any>()
-  const {t, i18n} = useTranslation();
+  const {t} = useTranslation();
   const [post, setPost] = useState<Post>();
-  const [additionalPosts, setAdditionalPosts] = useState<Post[]>();
   const [likes, setLikes] = React.useState<number>()
-  const [token,setToken] = React.useState('')
+  const [token, setToken] = React.useState('')
   const [dataSourceCords, setDataSourceCords] = useState([]);
-  const [scrollToIndex,setScrollToIndex]= useState()
+  const [scrollToIndex, setScrollToIndex] = useState()
   const [ref, setRef] = useState(null);
 
   React.useEffect(() => {
@@ -45,19 +42,16 @@ const ViewPost = (props:Props) => {
     setScrollToIndex(props.route.params.key)
   }, []);
 
-  useEffect(()=>{
-    //console.log(scrollToIndex)
-    //console.log(ref)
+  useEffect(() => {
     console.log(dataSourceCords)
-    if(dataSourceCords.length>0 && scrollToIndex && ref) {
+    if (dataSourceCords.length > 0 && scrollToIndex && ref) {
       console.log("inside use effect")
       scrollHandler()
-    }
-    else{
+    } else {
       console.log("else")
     }
 
-  }, [dataSourceCords,setDataSourceCords,scrollToIndex,ref])
+  }, [dataSourceCords, setDataSourceCords, scrollToIndex, ref])
 
 
   const scrollHandler = () => {
@@ -71,7 +65,7 @@ const ViewPost = (props:Props) => {
   };
   const [getPost] = useLazyQuery(FIND_POST_BY_ID, {
     context: {
-      headers: {'Authorization' : 'Bearer ' + token}
+      headers: {'Authorization': 'Bearer ' + token}
     },
     onCompleted: (data) => {
       setPost(data.findPostById)
@@ -83,33 +77,28 @@ const ViewPost = (props:Props) => {
   })
 
   React.useEffect(() => {
-    if(token != ''){
-      if(props.post) {
+    if (token != '') {
+      if (props.post) {
         setPost(props.post)
         setLikes(props.post.upvotes)
-      }
-      else {
-        getPost({variables: {id:props.route.params?.postId}})
+      } else {
+        getPost({variables: {id: props.route.params?.postId}})
       }
     }
   }, [token])
 
-  React.useEffect(() => {
-    if (props.additionalPosts) setAdditionalPosts(props.additionalPosts);
-    else if (props.route.params?.additionalPosts) setAdditionalPosts(props.route.params.additionalPosts);
-  }, [props.route.params, props.additionalPosts])
-
   const [getOwnerData, {data: ownerData}] = useLazyQuery(NEW_FIND_USER_BY_ID, {
     context: {
-      headers: {'Authorization' : 'Bearer ' + token}
+      headers: {'Authorization': 'Bearer ' + token}
     },
     onCompleted: (data) => {
       setOwner(data.findUserById.user);
-   },
-  onError: error => {
+    },
+    onError: error => {
       console.log('view post error');
       console.log(error);
-  }});
+    }
+  });
 
   const [like] = useMutation(LIKE_POST, {
     onCompleted: () => {
@@ -118,7 +107,7 @@ const ViewPost = (props:Props) => {
     },
     refetchQueries: [],
     context: {
-      headers: {'Authorization' : 'Bearer ' + token}
+      headers: {'Authorization': 'Bearer ' + token}
     }
   });
   const [unlike] = useMutation(UNLIKE_POST, {
@@ -128,22 +117,11 @@ const ViewPost = (props:Props) => {
     },
     refetchQueries: [],
     context: {
-      headers: {'Authorization' : 'Bearer ' + token}
+      headers: {'Authorization': 'Bearer ' + token}
     }
   });
 
-  const styles = StyleSheet.create({
-    button: {
-      position: 'absolute',
-      zIndex: 2,
-      top: 0,
-      left: 0,
-      padding: 0,
-      margin: 0
-    }
-  })
-
-  const likePost = (isLiking: boolean)  => {
+  const likePost = (isLiking: boolean) => {
     if (isLiking) {
       setLikes(likes + 1)
       like({variables: {userId: userId, postId: post.id}});
@@ -163,7 +141,7 @@ const ViewPost = (props:Props) => {
   useEffect(() => {
     if (post && post.owner) {
       getUserId().then(id => {
-        getOwnerData({variables: {targetUserId: post.owner.id? post.owner.id : post.owner}});
+        getOwnerData({variables: {targetUserId: post.owner.id ? post.owner.id : post.owner}});
         setUserId(id);
         if (post.owner.id == id) setLiked(true);
       })
@@ -173,98 +151,25 @@ const ViewPost = (props:Props) => {
   }, [post])
 
   const myIcon = <Icon type={'ionicon'} name={'ellipsis-horizontal'} style={{marginRight: 10}} {...props}/>
-  const LeftContent = props => <Avatar.Text style={{width: 50, height: 50, borderRadius: 50, backgroundColor: colors.extra}} label={owner && (owner.name[0] + owner.lastname[0])} {...props}/>
+  const LeftContent = props => <Avatar.Text
+    style={{width: 50, height: 50, borderRadius: 50, backgroundColor: colors.extra}}
+    label={owner && (owner.name[0] + owner.lastname[0])} {...props}/>
   const RightContent = () => <OptionsMenu
     customButton={myIcon}
     destructiveIndex={0}
     options={[t('view-post.report'), t('view-post.copy-link'), t('view-post.disconnect'), t('view-post.cancel')]}
-    actions={[()=>{console.log("TODO Report Post")}, ()=>{Clipboard.setString(Linking.createURL('post', {
-      queryParams: { id: post.id },
-    }))}, ()=>{console.log("TODO Disconnect to user")},()=>{}]}/>
+    actions={[() => {
+      console.log("TODO Report Post")
+    }, () => {
+      Clipboard.setString(Linking.createURL('post', {
+        queryParams: {id: post.id},
+      }))
+    }, () => {
+      console.log("TODO Disconnect to user")
+    }, () => {
+    }]}/>
 
-  /*const getPostCard = (post) => {
-    return (
-        <View >
-        <Card style={{backgroundColor: colors.background, borderRadius: 20, marginHorizontal: 10, marginTop: 10}}>
-          <TouchableOpacity onPress={() => {
-            props.navigation.navigate('profile', {otherId: post.owner?.id ? post.owner?.id : post.owner})
-          }} style={{backgroundColor: 'transparent', marginRight: 20}}>
-            <Card.Title subtitleStyle={{color: colors.primary, fontFamily: 'sans-serif-medium'}}
-                        title={<Text style={{
-                          fontWeight: 'bold',
-                          color: colors.primary,
-                          fontSize: 20,
-                          fontFamily: 'sans-serif-medium'
-                        }}>{owner && owner.mail}</Text>}
-                        subtitle={post.creationDate}
-                        left={LeftContent}
-                        right={RightContent}/>
-          </TouchableOpacity>
-          <Card.Content style={{marginHorizontal: 7, marginBottom: 10}}>
-            <Text style={{
-              fontSize: 20, color: colors.primary,
-              marginTop: 5, fontWeight: 'bold'
-            }}>{post.title}</Text>
-            <Paragraph style={{color: colors.primary, fontSize: 17, marginBottom: 5}}>{post.text}</Paragraph>
-          </Card.Content>
-          {(post.image && post.image !== "") && <Card.Cover style={{marginHorizontal: 15, borderRadius: 20}}
-                                                            resizeMode={'cover'}
-                                                            source={post.image ? {uri: post.image.replace('127.0.0.1', ip)} : require('../../assets/images/background/dots-background.png')}/>}
-          <Card.Actions style={{width: '100%', display: 'flex', justifyContent: 'space-between', marginVertical: 10}}>
-            <View style={{
-              display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'center',
-              marginLeft: 15,
-              backgroundColor: 'rgba(0,0,0,0)'
-            }}>
-              <IconButton disabled={post.owner?.id == userId} icon={liked ? 'heart' : 'heart-outline'}
-                          onPress={() => likePost(!liked)}/>
-              <Text style={{marginRight: 10, color: colors.primary}}> {likes} </Text>
-            </View>
-            <View style={{marginRight: 15, backgroundColor: 'rgba(0,0,0,0)'}}>
-              <Icon name={'share-variant'} style={{color: colors.primary}} type={'material-community'} onPress={() => {
-                let redirectUrl = Linking.createURL('tabbar/post', {
-                  queryParams: { id: post.id },
-                });
-                share(redirectUrl);
-              }}/>
-            </View>
-          </Card.Actions>
-        </Card>
-    )
-  }
-              {/!*<Icon name={'chat-outline'} type={'material-community'} style={{color: colors.primary}} onPress={() => {}}/>*!/}
-              {/!*<Text style={{color: colors.primary}}> 1 </Text>*!/}
-            </View>
-            <View style={{marginRight: 15, backgroundColor: 'rgba(0,0,0,0)'}}>
-              <Icon name={'share-variant'} style={{color: colors.primary}} type={'material-community'} onPress={() => {
-                let redirectUrl = Linking.createURL('tabbar/post', {
-                  queryParams: { id: post.id },
-                });
-                share(redirectUrl);
-              }}/>
-            </View>
-          </Card.Actions>
-          {/!*<Modal animationType="fade"*!/}
-          {/!*       presentationStyle={"fullScreen"}*!/}
-          {/!*       visible={viewProfile}*!/}
-          {/!*       onRequestClose={() => {*!/}
-          {/!*         setViewProfile(!viewProfile);*!/}
-          {/!*       }}>*!/}
-          {/!*  <IconButton onPress={() => setViewProfile(false)}*!/}
-          {/!*              icon={'chevron-left'}*!/}
-          {/!*              style={[styles.button, Platform.OS === 'ios' ? {marginTop: 15}: {}]}*!/}
-          {/!*              size={40}*!/}
-          {/!*  />*!/}
-          {/!*  <Profile navigation={props.navigation} otherUserId={typeof post.owner === "string" ? post.owner : post.owner.id}/>*!/}
-          {/!*</Modal>*!/}
-        </Card>
-        </View>
-    )
-  }*/
-
-  return (<View style={{ backgroundColor: colors.surface}}>
+  return (<View style={{backgroundColor: colors.surface}}>
     <ScrollView ref={(ref) => {
       setRef(ref);
     }} style={{
@@ -274,20 +179,14 @@ const ViewPost = (props:Props) => {
       overflow: "visible"
     }}>
       {props.route.params.additionalPosts.map((post, i) =>
-        <View   onLayout={(event) => {
-        const layout = event.nativeEvent.layout;
-        let aux= [...dataSourceCords]
-        aux[i]=layout.y
-        // dataSourceCords[i] = layout.y;
-        setDataSourceCords(aux);
-        /*  console.log(dataSourceCords);
-          console.log('height:', layout.height);
-          console.log('width:', layout.width);
-          console.log('x:', layout.x);*/
-        console.log('y:', layout.y);
+        <View onLayout={(event) => {
+          const layout = event.nativeEvent.layout;
+          let aux = [...dataSourceCords]
+          aux[i] = layout.y
+          setDataSourceCords(aux);
 
-      }} key={i} style={{marginBottom: 5,backgroundColor: colors.surface}}>
-          <View style={{ backgroundColor: colors.surface}}>
+        }} key={i} style={{marginBottom: 5, backgroundColor: colors.surface}}>
+          <View style={{backgroundColor: colors.surface}}>
             <Card style={{backgroundColor: colors.background, borderRadius: 20, marginHorizontal: 10, marginTop: 10}}>
               <TouchableOpacity onPress={() => {
                 props.navigation.navigate('profile', {otherId: post.owner?.id ? post.owner?.id : post.owner})
@@ -312,7 +211,8 @@ const ViewPost = (props:Props) => {
               </Card.Content>
               {(post.image && post.image !== "") && <Card.Cover style={{marginHorizontal: 15, borderRadius: 20}}
                                                                 source={require('../../assets/images/post.jpg')}/>}
-              <Card.Actions style={{width: '100%', display: 'flex', justifyContent: 'space-between', marginVertical: 10}}>
+              <Card.Actions
+                style={{width: '100%', display: 'flex', justifyContent: 'space-between', marginVertical: 10}}>
                 <View style={{
                   display: 'flex',
                   flexDirection: 'row',
@@ -323,37 +223,23 @@ const ViewPost = (props:Props) => {
                   <IconButton disabled={post.owner?.id == userId} icon={liked ? 'heart' : 'heart-outline'}
                               onPress={() => likePost(!liked)}/>
                   <Text style={{marginRight: 10, color: colors.primary}}> {likes} </Text>
-                  {/*<Icon name={'chat-outline'} type={'material-community'} style={{color: colors.primary}} onPress={() => {}}/>*/}
-                  {/*<Text style={{color: colors.primary}}> 1 </Text>*/}
                 </View>
                 <View style={{marginRight: 15, backgroundColor: 'rgba(0,0,0,0)'}}>
-                  <Icon name={'share-variant'} style={{color: colors.primary}} type={'material-community'} onPress={() => {
-                    let redirectUrl = Linking.createURL('post', {
-                      queryParams: { id: post.id },
-                    });
-                    share(redirectUrl);
-                  }}/>
+                  <Icon name={'share-variant'} style={{color: colors.primary}} type={'material-community'}
+                        onPress={() => {
+                          let redirectUrl = Linking.createURL('post', {
+                            queryParams: {id: post.id},
+                          });
+                          share(redirectUrl);
+                        }}/>
                 </View>
               </Card.Actions>
-              {/*<Modal animationType="fade"*/}
-              {/*       presentationStyle={"fullScreen"}*/}
-              {/*       visible={viewProfile}*/}
-              {/*       onRequestClose={() => {*/}
-              {/*         setViewProfile(!viewProfile);*/}
-              {/*       }}>*/}
-              {/*  <IconButton onPress={() => setViewProfile(false)}*/}
-              {/*              icon={'chevron-left'}*/}
-              {/*              style={[styles.button, Platform.OS === 'ios' ? {marginTop: 15}: {}]}*/}
-              {/*              size={40}*/}
-              {/*  />*/}
-              {/*  <Profile navigation={props.navigation} otherUserId={typeof post.owner === "string" ? post.owner : post.owner.id}/>*/}
-              {/*</Modal>*/}
             </Card>
           </View>
         </View>
       )}
 
-  </ScrollView>
+    </ScrollView>
 
   </View>)
 
